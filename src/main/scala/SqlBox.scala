3a23916be9b59c3f4d572f5a1a185e548823d5d1
Luo Yong <luo.yong.name@gmail.com>
2017-02-11 22:52:04 +0800

    add SqlBox

---
+++ src/main/scala/SqlBox.scala
@@ -0,0 +1,48 @@
+import java.sql.{Connection, PreparedStatement}
+
+/**
+  *
+  */
+class SqlBox {
+
+  val _paramList = collection.mutable.ListBuffer[Any]()
+  var _sql:String = null
+
+  def checkParam(condition:Boolean, params:Any*):Boolean = {
+    if (condition) {
+      params.foreach((param:Any) => {_paramList += param})
+    }
+    return condition
+  }
+
+  def checkIfMapContainsKey(params:Map[String, Any], keys:String*):Boolean = {
+    var checkResult = true
+    keys.foreach((key) => {
+      if (!params.contains(key)) {
+        checkResult = false
+      }
+    })
+    if (checkResult) {
+      keys.foreach((key) => {
+        _paramList += params(key)
+      })
+      return true
+    }else {
+      return false
+    }
+  }
+
+  def paramList:List[Any] = this._paramList.toList
+
+  def sql_=(sql:String) = {
+    _sql = sql
+  }
+
+  def sql = {
+    _sql
+  }
+
+  def setPreparedStatementParams(stmt:PreparedStatement) = {
+    (1 to _paramList.size).foreach((idx) => {stmt.setObject(idx, _paramList(idx-1))})
+  }
+}
